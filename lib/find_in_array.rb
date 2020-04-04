aca5ad7cc5ce855ad73b1bf7dd610f8cdd15418f
Phyllis N <phyllisgetcity@gmail.com>
2020-04-04 20:32:15 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
       counter += 1 
   end 
   if array[counter] == value_to_find
-    return array[counter]
+    return counter
   else 
     return nil
   end 
